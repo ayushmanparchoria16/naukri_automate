02805bd4e435368604b6f1275822157c8a19da23
Ayushman Parchhoria <ayushmanparchoria16@gmail.com>
2026-09-05 03:47:37 +0530

    Add explicit waits for page elements

--- naukri_update.py
+++ naukri_update.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import os, time
 
 options = Options()
@@ -9,16 +11,18 @@ options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
 driver = webdriver.Chrome(options=options)
+wait = WebDriverWait(driver, 15)
 driver.get("https://www.naukri.com/nlogin/login")
 
-driver.find_element(By.ID, "usernameField").send_keys(os.getenv("NAUKRI_USER"))
-driver.find_element(By.ID, "passwordField").send_keys(os.getenv("NAUKRI_PASS"))
-driver.find_element(By.XPATH, "//button[text()='Login']").click()
+wait.until(EC.presence_of_element_located((By.ID, "usernameField"))).send_keys(os.getenv("NAUKRI_USER"))
+wait.until(EC.presence_of_element_located((By.ID, "passwordField"))).send_keys(os.getenv("NAUKRI_PASS"))
+wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Login']"))).click()
 
 time.sleep(5)
 driver.get("https://www.naukri.com/mnjuser/profile")
 
 # Trigger profile update by clicking Save
-driver.find_element(By.XPATH, "//button[text()='Save']").click()
+wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Save']"))).click()
 
+time.sleep(3) # wait a few seconds for the save to process before quitting
 driver.quit()
